a73cadbb27b177cbc06f01a5dad360fe9ddf5616
raghavendra-musubi <raghavendra@techis.io>
2021-02-24 23:17:01 +0530

    movie card

--- client/src/components/MovieCategory.js
+++ client/src/components/MovieCategory.js
@@ -2,15 +2,15 @@ import React, { Fragment } from "react";
 import MovieTile from './MovieTile'
 
 function MovieCategory({ categoryName, categoryMovies }) {
-  console.log("LOG 4: ", categoryName, categoryMovies);
+
   return (
     <Fragment>
-      <div className="my-5">
+      <div className="my-5 container">
         <p>
           <b className='text-light'>{categoryName.toUpperCase()}</b>
         </p>
         <hr className='border border-light'/>
-        <div className='d-flex flex-row'>
+        <div className='d-flex flex-row flex-wrap justify-content-around'>
             {categoryMovies.map( movie => <MovieTile key={movie.id} movie={movie}/> )}
         </div>
       </div>
--- client/src/components/MovieTile.js
+++ client/src/components/MovieTile.js
@@ -1,11 +1,17 @@
-import React, { Fragment } from 'react'
+import React, { Fragment } from "react";
 
-function MovieTile({movie}) {
-    return <Fragment>
-        <div className='card m-3'>
-            <b>{movie.name}</b>
+function MovieTile({ movie }) {
+  console.log("LOG 6: ", movie);
+  return (
+    <Fragment>
+      <div className="card m-3 movie-tile">
+        <div className="card-title">
+          <b>{movie.name}</b>
         </div>
+        <img src={movie.image_path} height={150} width={200} />
+      </div>
     </Fragment>
+  );
 }
 
-export default MovieTile
+export default MovieTile;
